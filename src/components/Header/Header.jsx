b6153abcd9032519588460cd307c81885b1df2c7
Viacheslav Liabin <R9514633542@gmail.com>
2023-10-08 23:54:22 +0500

    first

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/Header.module.css";
+
+import { ROUTES } from "../../utils/routes";
+
+import LOGO from "../../images/logo.svg";
+import AVATAR from "../../images/avatar.jpg";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={LOGO} alt="logo" />
+        </Link>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div
+            className={styles.avatar}
+            style={{ backgroundImage: `url(${AVATAR})` }}
+          ></div>
+          <div className={styles.username}>Guest</div>
+        </div>
+        <form className={styles.form}>
+          <div className={styles.icon}>
+            <svg className="icon">
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#search`} />
+            </svg>
+          </div>
+          <div className={styles.input}>
+            <input
+              type="search"
+              name="search"
+              placeholder="Search for anything... "
+              autoComplete="off"
+              onChange={() => {}}
+              value={}
+            />
+          </div>
+          <div className={styles.box}></div>
+        </form>
+
+        <div className={styles.account}>
+          <Link to={ROUTES.HOME} className={styles.favourites}>
+            {/* 20.56 */}
+          <svg className={styles["icon-fav"]}>
+              <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#heart`} />
+            </svg>
+          </Link>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
