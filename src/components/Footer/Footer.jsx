5c386232b3e47ddfcd5e50ad1655d682d3916aea
Viacheslav Liabin <R9514633542@gmail.com>
2023-11-13 20:17:02 +0500

    fillHeaderFooter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../../styles/Footer.module.css";
-import { Link } from "react-router-dom";
 import { ROUTES } from "../../utils/routes";
 
 import LOGO from "../../images/logo.svg";
 
-const Footer = () => {
-  return (
-    <section className={styles.footer}>
-      <div className={styles.logo}>
-        <Link to={ROUTES.HOME}>
-          <img src={LOGO} alt="Stuff" />
-        </Link>
-      </div>
-      <div className={styles.rights}>Developed by SlavikLyabin</div>
-      <div className={styles.socials}>
-        <a href="https://instagram.com" target="_blank" rel="noreferrer">
-          
-        </a>
-      </div>
-    </section>
-  );
-};
+const Footer = () => (
+  <section className={styles.footer}>
+    <div className={styles.logo}>
+      <Link to={ROUTES.HOME}>
+        <img src={LOGO} alt="Stuff" />
+      </Link>
+    </div>
+
+    <div className={styles.rights}>
+      Developed by{" "}
+      <a
+        href="https://chelyabinsk.hh.ru/resume/d06086d7ff0c20ccbb0039ed1f637659617244"
+        target="_blank"
+        rel="noreferrer"
+      >
+        SlavikLiabin
+      </a>
+    </div>
+
+    <div className={styles.socials}>
+      <a href="https://instagram.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`} />
+        </svg>
+      </a>
+
+      <a href="https://facebook.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`} />
+        </svg>
+      </a>
+
+      <a href="https://youtube.com" target="_blank" rel="noreferrer">
+        <svg className="icon">
+          <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`} />
+        </svg>
+      </a>
+    </div>
+  </section>
+);
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import styles from "../../styles/Header.module.css";
 
@@ -8,22 +9,52 @@ import { ROUTES } from "../../utils/routes";
 import LOGO from "../../images/logo.svg";
 import AVATAR from "../../images/avatar.jpg";
 
+import { toggleForm } from "../../features/user/userSlice";
+import { useGetProductsQuery } from "../../features/api/apiSlice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [searchValue, setSearchValue] = useState("");
+  const { currentUser, cart } = useSelector(({ user }) => user);
+
+  const [values, setValues] = useState({ name: "Guest", avatar: AVATAR });
+
+  const { data, isLoading } = useGetProductsQuery({ title: searchValue });
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    setValues(currentUser);
+  }, [currentUser]);
+
+  const handleClick = () => {
+    if (!currentUser) dispatch(toggleForm(true));
+    else navigate(ROUTES.PROFILE);
+  };
+
+  const handleSearch = ({ target: { value } }) => {
+    setSearchValue(value);
+  };
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
         <Link to={ROUTES.HOME}>
-          <img src={LOGO} alt="logo" />
+          <img src={LOGO} alt="Stuff" />
         </Link>
       </div>
+
       <div className={styles.info}>
-        <div className={styles.user}>
+        <div className={styles.user} onClick={handleClick}>
           <div
             className={styles.avatar}
-            style={{ backgroundImage: `url(${AVATAR})` }}
-          ></div>
-          <div className={styles.username}>Guest</div>
+            style={{ backgroundImage: `url(${values.avatar})` }}
+          />
+          <div className={styles.username}>{values.name}</div>
         </div>
+
         <form className={styles.form}>
           <div className={styles.icon}>
             <svg className="icon">
@@ -34,13 +65,37 @@ const Header = () => {
             <input
               type="search"
               name="search"
-              placeholder="Search for anything... "
+              placeholder="Search for anyting..."
               autoComplete="off"
-              onChange={() => {}}
-              value=""
+              onChange={handleSearch}
+              value={searchValue}
             />
           </div>
-          {false && <div className={styles.box}></div>}
+
+          {searchValue && (
+            <div className={styles.box}>
+              {isLoading
+                ? "Loading"
+                : !data.length
+                ? "No results"
+                : data.map(({ title, images, id }) => {
+                    return (
+                      <Link
+                        key={id}
+                        onClick={() => setSearchValue("")}
+                        className={styles.item}
+                        to={`/products/${id}`}
+                      >
+                        <div
+                          className={styles.image}
+                          style={{ backgroundImage: `url(${images[0]})` }}
+                        />
+                        <div className={styles.title}>{title}</div>
+                      </Link>
+                    );
+                  })}
+            </div>
+          )}
         </form>
 
         <div className={styles.account}>
@@ -54,7 +109,9 @@ const Header = () => {
             <svg className={styles["icon-cart"]}>
               <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#bag`} />
             </svg>
-            <span className={styles.count}>2</span>
+            {!!cart.length && (
+              <span className={styles.count}>{cart.length}</span>
+            )}
           </Link>
         </div>
       </div>
